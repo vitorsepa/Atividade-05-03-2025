4db4b0ff8414a2ba9dce01788e011ea0ad6ec063
VITOR COSTA CARDOSO DA LUZ <vitor_c_luz@estudante.sesisenai.org.br>
2025-03-05 09:20:14 -0300

    inicializando js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-a=1
+// pegar o elemento canvas pelo id
+const canvas = document.getElementById('jogo2d')
+
+// inicializar o canvas
+const ctx = canvas.getContext('2d')
+
+// criar função loop que executa 60 vezes por segundo
